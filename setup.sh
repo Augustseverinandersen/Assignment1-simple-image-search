e3e28fa264c008a4deb4a38552fdd3b79907b54f
Augustseverinandersen <augustseverinandersen@hotmail.com>
2023-04-07 14:22:46 +0200

    made functions

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 
 # Run the code 
-python3 code/image_search_algorithm.py --image_name image_0201.jpg --directory data/flowers/flowers
+python3 src/image_search_algorithm.py --image_name image_0201.jpg --directory data/flowers/flowers
 
 # Deactivate the virtual environment.
 deactivate
